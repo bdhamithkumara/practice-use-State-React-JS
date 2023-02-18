8132d9252805d6c88d0fd969cbd4e92203d3da6b
Damithkumara <damithkumararoxx55@gmail.com>
2023-02-18 21:47:02 +0530

    added IsVisible

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,12 +4,15 @@ const Home = () => {
 
     const [count , setCount] = React.useState(0);
 
-    const [btnname , setBtnname] = React.useState("Click on me!")
+    const [btnname , setBtnname] = React.useState("Click on me!");
+
+    const [isVisible, setIsVisible] = React.useState(false);
 
     const addCountTo = () => {
        let newCount = count + 1;
        setCount(newCount);
        setBtnname("Add to Count");
+       setIsVisible(true);
     };
 
   return (
@@ -20,8 +23,10 @@ const Home = () => {
         textAlign : "center",
         fontSize : "40px",
         }}>
-
-            <p>I've been clicked <span>{count}</span> time(s)!</p>
+            {isVisible && 
+                (<p>I've been clicked <span>{count}</span> time(s)!</p>)
+            }
+            
 
         <button onClick={addCountTo} style={{padding:"20px",marginTop:"30px",fontSize:"20px"}}>
             {btnname}
