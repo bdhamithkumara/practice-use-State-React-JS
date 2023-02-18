e5d600bc40d35ce6221ff35aaa9f624e16bb0586
Damithkumara <damithkumararoxx55@gmail.com>
2023-02-18 21:41:25 +0530

    change the count value

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
 
 const Home = () => {
+
+    const [count , setCount] = React.useState(0);
+
+    const addCountTo = () => {
+       let newCount = count + 1;
+       setCount(newCount);
+    };
+
   return (
-    <div>Home</div>
+    <div style={{
+        marginLeft:"auto",
+        marginRight : "auto",
+        alignContent : "center",
+        textAlign : "center",
+        fontSize : "40px",
+        }}>
+
+            <p>I've been clicked <span>{count}</span> time(s)!</p>
+
+        <button onClick={addCountTo} style={{padding:"20px",marginTop:"30px",fontSize:"20px"}}>Add to count</button>
+    </div>
   )
 }
 
